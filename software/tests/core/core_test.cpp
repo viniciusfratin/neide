09e9cf4ff7edc81cda36e995766527baddf4486b
viniciusfratin <viniciusfratin@hotmail.com>
2022-04-10 14:42:24 -0300

    Changed all tabs to 4 spaces.

--- software/tests/core/core_test.cpp
+++ software/tests/core/core_test.cpp
@@ -7,79 +7,79 @@ extern "C"
 
 Bool stub_should_wake_up_true()
 {
-  return TRUE;
+	return TRUE;
 }
 
 Bool stub_should_wake_up_false()
 {
-  return FALSE;
+	return FALSE;
 }
 
 TEST(CoreTest, ShouldBeIdleStateAfterInitialization)
 {
-  /* Given */
-  SystemCore system_core = SystemCore_Construct(stub_should_wake_up_true);
-  
-  /* When */
-  CoreState current_state = SystemCore_GetCurrentState(system_core);
+	/* Given */
+	SystemCore system_core = SystemCore_Construct(stub_should_wake_up_true);
 
-  /* Then */
-  EXPECT_EQ(current_state, CORE_STATE_IDLE);
+	/* When */
+	CoreState current_state = SystemCore_GetCurrentState(system_core);
+
+	/* Then */
+	EXPECT_EQ(current_state, CORE_STATE_IDLE);
 
 
-  SystemCore_Destruct(&system_core);
+	SystemCore_Destruct(&system_core);
 }
 
 TEST(CoreTest, ShouldBeNotValidAfterDestruction)
 {
-  /* Given */
-  SystemCore system_core = SystemCore_Construct(stub_should_wake_up_true);
-  
-  /* When */
-  SystemCore_Destruct(&system_core);
+	/* Given */
+	SystemCore system_core = SystemCore_Construct(stub_should_wake_up_true);
 
-  /* Then */
-  EXPECT_EQ(system_core, SYSTEM_CORE_INVALID_INSTANCE);
+	/* When */
+	SystemCore_Destruct(&system_core);
+
+	/* Then */
+	EXPECT_EQ(system_core, SYSTEM_CORE_INVALID_INSTANCE);
 }
 
 TEST(CoreTest, ShouldBeValidAfterConstruction)
 {
-  /* Given, when */
-  SystemCore system_core = SystemCore_Construct(stub_should_wake_up_true);
-  
-  /* Then */
-  EXPECT_NE(system_core, SYSTEM_CORE_INVALID_INSTANCE);
+	/* Given, when */
+	SystemCore system_core = SystemCore_Construct(stub_should_wake_up_true);
+
+	/* Then */
+	EXPECT_NE(system_core, SYSTEM_CORE_INVALID_INSTANCE);
 
 
-  SystemCore_Destruct(&system_core);
+	SystemCore_Destruct(&system_core);
 }
 
 TEST(CoreTest, ShouldBeWokeStateWhenIdleStateAndAdvancingCycleAndWakeUpCallbackIsTrue)
 {
-  /* Given */
-  SystemCore system_core = SystemCore_Construct(stub_should_wake_up_true);
+	/* Given */
+	SystemCore system_core = SystemCore_Construct(stub_should_wake_up_true);
 
-  /* When */
-  SystemCore_AdvanceCycle(system_core);
+	/* When */
+	SystemCore_AdvanceCycle(system_core);
 
-  /* Then */
-  EXPECT_EQ(SystemCore_GetCurrentState(system_core), CORE_STATE_WOKE);
+	/* Then */
+	EXPECT_EQ(SystemCore_GetCurrentState(system_core), CORE_STATE_WOKE);
 
 
-  SystemCore_Destruct(&system_core);
+	SystemCore_Destruct(&system_core);
 }
 
 TEST(CoreTest, ShouldBeIdleStateWhenIdleStateAndAdvancingCycleAndWakeUpCallbackIsFalse)
 {
-  /* Given */
-  SystemCore system_core = SystemCore_Construct(stub_should_wake_up_false);
+	/* Given */
+	SystemCore system_core = SystemCore_Construct(stub_should_wake_up_false);
 
-  /* When */
-  SystemCore_AdvanceCycle(system_core);
+	/* When */
+	SystemCore_AdvanceCycle(system_core);
 
-  /* Then */
-  EXPECT_EQ(SystemCore_GetCurrentState(system_core), CORE_STATE_IDLE);
+	/* Then */
+	EXPECT_EQ(SystemCore_GetCurrentState(system_core), CORE_STATE_IDLE);
 
 
-  SystemCore_Destruct(&system_core);
+	SystemCore_Destruct(&system_core);
 }
