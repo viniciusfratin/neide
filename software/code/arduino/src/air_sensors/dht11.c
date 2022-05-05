d2cbd039d62a248031f3eac2f6a7ce26ab7a3ba2
viniciusfratin <viniciusfratin@hotmail.com>
2022-05-04 21:57:33 -0300

    Added based-on credits.

--- software/code/arduino/src/air_sensors/dht11.c
+++ software/code/arduino/src/air_sensors/dht11.c
@@ -1,3 +1,12 @@
+/*
+ * DHT Sensor for AVR-GCC Family
+ *
+ * Author      : David Feng
+ * Updated     : June 26th, 2018
+ * Description : DHT Sensor Library for AVR-GCC compiler
+ * Usage       : Use library with AVR-GCC for Atmega family of microcontrollers (tested with Atmega 1284)
+ */
+
 #include "dht11.h"
 #include "common.h"
 #include "pin_utils/gpio_utils.h"
