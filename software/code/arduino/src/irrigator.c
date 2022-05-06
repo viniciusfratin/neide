f218a17d283a2065c403a21a448dcb1b83d1ee00
viniciusfratin <viniciusfratin@hotmail.com>
2022-05-06 01:52:34 -0300

    Updated irrigator code.

--- software/code/arduino/src/irrigator.c
+++ software/code/arduino/src/irrigator.c
@@ -1,17 +1,22 @@
 #include "irrigator.h"
 #include "irrigator_interface.h"
 #include "irrigator_interface_construction.h"
+#include "pin_utils/gpio_utils.h"
 #include <stdlib.h>
 
 typedef struct IrrigatorInternal
 {
     IrrigatorInterface irrigator_interface;
     int last_irrigation_time;
+    GetCurrentTimeSecondsCallback get_current_time_seconds_callback;
+    volatile uint8_t* pin_ddr_ptr;
+    volatile uint8_t* pin_port_ptr; 
+    uint8_t pin;
 } IrrigatorImplementation;
 
 static void Irrigator_Irrigate(void* object_instance, int irrigation_time_seconds);
 
-Irrigator Irrigator_Construct()
+Irrigator Irrigator_Construct(GetCurrentTimeSecondsCallback get_current_time_seconds_callback, volatile uint8_t* pin_ddr_ptr, volatile uint8_t* pin_port_ptr, uint8_t pin)
 {
     Irrigator instance = (Irrigator)malloc(sizeof(IrrigatorImplementation));
 
@@ -24,7 +29,14 @@ Irrigator Irrigator_Construct()
 
         if(instance->irrigator_interface != IRRIGATOR_INTERFACE_INVALID_INSTANCE)
         {
-            instance->last_irrigation_time = -1;
+            instance->last_irrigation_time = 0;
+            instance->get_current_time_seconds_callback = get_current_time_seconds_callback;
+            instance->pin_ddr_ptr = pin_ddr_ptr;
+            instance->pin_port_ptr = pin_port_ptr;
+            instance->pin = pin;
+
+            SET_GPIO_PIN_AS_OUTPUT(instance->pin_ddr_ptr, instance->pin);
+            SET_GPIO_PIN_TO_LOW(instance->pin_port_ptr, instance->pin);
         }
         else
         {
@@ -55,15 +67,21 @@ IrrigatorInterface Irrigator_GetIrrigatorInterface(Irrigator instance)
     return instance->irrigator_interface;
 }
 
-int Irrigator_GetLastIrrigationTime(Irrigator instance)
+int Irrigator_GetTimeFromLastIrrigation(Irrigator instance)
 {
-    return instance->last_irrigation_time;
+    int current_time = instance->get_current_time_seconds_callback();
+
+    return (current_time - instance->last_irrigation_time);
 }
 
 static void Irrigator_Irrigate(void* object_instance, int irrigation_time_seconds)
 {
     Irrigator instance = (Irrigator)object_instance;
-    instance->last_irrigation_time = irrigation_time_seconds;
+    instance->last_irrigation_time = instance->get_current_time_seconds_callback();
+
+    SET_GPIO_PIN_TO_HIGH(instance->pin_port_ptr, instance->pin);
+    while((instance->get_current_time_seconds_callback() - instance->last_irrigation_time) <= irrigation_time_seconds);
+    SET_GPIO_PIN_TO_LOW(instance->pin_port_ptr, instance->pin);
 }
 
 
--- software/code/arduino/src/irrigator.h
+++ software/code/arduino/src/irrigator.h
@@ -2,6 +2,8 @@
 #define IRRIGATOR_H
 
 #include "irrigator_interface.h"
+#include "clock/clock.h"
+#include <stdint.h>
 
 typedef struct IrrigatorInternal* Irrigator;
 
@@ -9,10 +11,10 @@ typedef struct IrrigatorInternal* Irrigator;
 
 
 IrrigatorInterface Irrigator_GetIrrigatorInterface(Irrigator instance);
-int Irrigator_GetLastIrrigationTime(Irrigator instance);
+int Irrigator_GetTimeFromLastIrrigation(Irrigator instance);
 
 
-Irrigator Irrigator_Construct();
+Irrigator Irrigator_Construct(GetCurrentTimeSecondsCallback get_current_time_seconds_callback, volatile uint8_t* pin_ddr_ptr, volatile uint8_t* pin_port_ptr, uint8_t pin);
 void Irrigator_Destruct(Irrigator* instancePtr);
 
 
