05148154aaf205006060958b4e4ea1f8cea8ac6a
viniciusfratin <viniciusfratin@hotmail.com>
2022-10-22 17:44:24 -0300

    Updated Jenkinsfile, Dockerfile and associated scripts.

--- scripts/build_target_and_copy_files.sh
+++ scripts/build_target_and_copy_files.sh
@@ -5,6 +5,6 @@ set -e
 rm -rf target_files
 docker build --tag viniciusfratin/neide_build_target:v0.1 --target neide_build_target --file ./docker/Dockerfile .
 docker run --name neide_build_target --rm --tty --detach viniciusfratin/neide_build_target:v0.1 bash
-docker cp neide_build_target:/neide_out .
+docker cp neide_build_target:/home/user/neide_out .
 docker stop neide_build_target
 mv neide_out target_files
